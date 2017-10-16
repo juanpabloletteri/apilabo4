c8486e00d22a57ba819033817c0b690cbdcf8f07
juanpabloletteri <pabloletteri@gmail.com>
2017-10-16 18:26:16 -0300

    nueva ruta

--- index.php
+++ index.php
@@ -33,7 +33,13 @@ $app->post('/agregarmaterial', function (Request $request, Response $response) {
     $password = $request->getParam("password");
     $response = persona::AgregarMaterial($nombre, $mail, $sexo, $password);
     return $response;
-});/////////////////////////////////////////
+});
+///////////////////////////////////
+$app->get('/tablamateriales', function (Request $request, Response $response) {  
+    $response = persona::traerTodos();
+    return $response;
+});
+//////////////////////////////////////////////
 
 $app->get('[/]', function (Request $request, Response $response) {
     $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework");
@@ -54,76 +60,6 @@ $app->delete('[/]', function (Request $request, Response $response) {
     $response->getBody()->write(" DELETE => Bienvenido!!! ,a SlimFramework");
     return $response;
 });
-
-
-
-$app->get('/datos/', function (Request $request, Response $response) {
-    $datos = array('nombre' => 'rogelio','apellido' => 'agua', 'edad' => 40);
-    $newResponse = $response->withJson($datos, 200);
-    return $newResponse;
-});
-
-$app->post('/datos/', function (Request $request, Response $response) {
-    $ArrayDeParametros = $request->getParsedBody();
-   // var_dump($ArrayDeParametros);
-    $objeto= new stdclass();
-    $objeto->nombre=$ArrayDeParametros['nombre'];
-    $objeto->apellido=$ArrayDeParametros['apellido'];
-    $objeto->edad=$ArrayDeParametros['edad'];
-    $newResponse = $response->withJson($objeto, 200);
-    return $newResponse;
-});
-
-/* atender todos los verbos de HTTP*/
-$app->any('/cualquiera/[{id}]', function ($request, $response, $args) {
-    
-    var_dump($request->getMethod());
-    $id=$args['id'];
-    $response->getBody()->write("cualquier verbo de ajax parametro: $id ");
-    return $response;
-});
-
-
-
-/* atender algunos los verbos de HTTP*/
-$app->map(['GET', 'POST'], '/mapeado', function ($request, $response, $args) {
-
-      var_dump($request->getMethod());
-     $response->getBody()->write("Solo POST y GET");
-});
-
-
-/* agrupacion de ruta*/
-$app->group('/saludo', function () {
-
-    $this->get('/{nombre}', function ($request, $response, $args) {
-        $nombre=$args['nombre'];
-        $response->getBody()->write("HOLA, Bienvenido <h1>$nombre</h1> a la apirest de 'CDs'");
-    });
-
-     $this->get('/', function ($request, $response, $args) {
-        $response->getBody()->write("HOLA, Bienvenido a la apirest de 'CDs'... ingresá tu nombre");
-     });
- 
-     $this->post('/', function ($request, $response, $args) {
-        $response->getBody()->write("HOLA, Bienvenido a la apirest por post");
-     });
-});
-
-
-/* agrupacion de ruta y mapeado*/
-$app->group('/usuario/{id:[0-9]+}', function () {
-
-    $this->map(['POST', 'DELETE'], '', function ($request, $response, $args) {
-        $response->getBody()->write("Borro el usuario por p");
-    });
-
-    $this->get('/nombre', function ($request, $response, $args) {
-        $response->getBody()->write("Retorno el nombre del usuario del id ");
-    });
-});
-
-
 
 
 /*LLAMADA A METODOS DE INSTANCIA DE UNA CLASE*/
